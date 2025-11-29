46cec610e1c8993c0cdd42e8fa4bd62d4216dbdb
Ika Mania <maniaika07@gmail.com>
2025-11-29 23:31:48 +0400

    chore: replace div with NavLink in AuthBox
    - Add relative links in Auth.tsx

--- frontend/src/components/auth/AuthBox.tsx
+++ frontend/src/components/auth/AuthBox.tsx
@@ -1,16 +1,20 @@
-interface AuthBoxProps {
+import { NavLink, type NavLinkProps } from "react-router-dom"
+
+interface AuthBoxProps extends NavLinkProps {
   text: string;
   css?: string;
 }
 
-const AuthBox = ({ text, css }: AuthBoxProps) => {
+const AuthBox = ({ text, css, ...props }: AuthBoxProps) => {
   return (
-    <div className={`
+    <NavLink
+      {...props}
+      className={`
         w-full h-[2.7rem] rounded-full flex justify-center items-center cursor-pointer
         my-[1rem] text-[.9rem] font-bold ${css ?? "border-1 border-gray-200"}
       `}>
       {text}
-    </div>
+    </NavLink>
   )
 }
 
